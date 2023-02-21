6153944e7f1cef70d1ccf46f0d20a4b76cb028c3
快乐的老鼠宝宝 <keaitianxinmail@qq.com>
2023-02-21 12:21:13 +0800

    init: get summary function
    总之就是非常不爽
    
    导师开催了，然后发现接口跑不通了
    什么情况啊（摔

---
+++ src/main.py
@@ -0,0 +1,14 @@
+import requests
+
+site_endpoint="app.cngal.org"
+
+def data_summary(tab="games", format="json"):
+    # format: csv/json/excel
+    # 吐槽，方块你怎么给我来个 blob:https://app.cngal.org/5e68bf9e-afcd-4184-b7b9-6a0fcb368c3e 这样的东西啊，没有固定的请求链接吗
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
