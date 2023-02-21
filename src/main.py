78246e60eac9327b4c14191684d089b2f5a0f225
快乐的老鼠宝宝 <keaitianxinmail@qq.com>
2023-02-21 17:16:37 +0800

    refactor: split main file to different modules

--- src/main.py
+++ src/main.py
@@ -1,51 +1,15 @@
-import json
-import os
-from pprint import pprint
+from src.const import api_endpoint
+from src.network import no_proxy, data_swagger_api
 
-import requests
-
-api_endpoint = "https://api.cngal.org"
-headers = {"User-Agent": "python-cngal /technical-preview"}
-
-
-def no_proxy(domain: str) -> None:
-    # If meet with this Error like this:
-    # requests.exceptions.SSLError: HTTPSConnectionPool(host='https://domain', port=443): Max retries exceeded with url:
-    # (Caused by SSLError(SSLEOFError(8, 'EOF occurred in violation of protocol (_ssl.c:997)')))
-    os.environ["NO_PROXY"] = domain
-
-
-def data_summary(tab="games"):
-    if tab == "games":
-        url = api_endpoint + "/api/tables/GetBasicInforList"
-    elif tab == "group":
-        url = api_endpoint + "/api/tables/GetGroupInforList"
-    elif tab == "staff":
-        url = api_endpoint + "/api/tables/GetStaffInforList"
-    elif tab == "maker":
-        url = api_endpoint + "/api/tables/GetMakerInforList"
-    elif tab == "character":
-        url = api_endpoint + "/api/tables/GetRoleInforList"
-    elif tab == "price":
-        url = api_endpoint + "/api/tables/GetSteamInforList"
-    elif tab == "score":
-        url = api_endpoint + "/api/tables/GetGameScoreList"
-    else:
-        print("数据汇总就这些分类没有其他的啦！写BUG了吧！")
-    result_json = json.loads(requests.get(url=url, headers=headers).text)
-    pprint(result_json)
-
-
-def data_swagger_api(api_name: str):
-    url = api_endpoint + api_name
-    result_json = json.loads(requests.get(url=url, headers=headers).text)
-    print(result_json)
-
+def init_graph():
+    # get data
+    data_swagger_api("/api/entries/GetAllEntriesIdName")
+    # complete data
+    data_swagger_api("/api/entries/GetEntryView/{id}")
 
 def main():
     no_proxy(api_endpoint)
-    # data_summary("maker")
-    data_swagger_api("/api/entries/GetAllEntriesIdName")
+
 
 
 if __name__ == "__main__":
