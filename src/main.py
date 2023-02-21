a5048257ea583a6222a8d4dac68811a908e6fd82
快乐的老鼠宝宝 <keaitianxinmail@qq.com>
2023-02-21 15:43:22 +0800

    feat: requests data summary list
    no_proxy Code imported from: https://github.com/OSMChina/Zhongli/blob/main/src/storagebox/requester.py#L13-L17
    
    summary page: https://app.cngal.org/data

--- src/main.py
+++ src/main.py
@@ -1,14 +1,40 @@
+import os
+
 import requests
 
-site_endpoint="app.cngal.org"
+api_endpoint = "https://api.cngal.org/api/"
 
-def data_summary(tab="games", format="json"):
-    # format: csv/json/excel
-    # 吐槽，方块你怎么给我来个 blob:https://app.cngal.org/5e68bf9e-afcd-4184-b7b9-6a0fcb368c3e 这样的东西啊，没有固定的请求链接吗
+
+def no_proxy(domain: str) -> None:
+    # If meet with this Error like this:
+    # requests.exceptions.SSLError: HTTPSConnectionPool(host='https://domain', port=443): Max retries exceeded with url:
+    # (Caused by SSLError(SSLEOFError(8, 'EOF occurred in violation of protocol (_ssl.c:997)')))
+    os.environ["NO_PROXY"] = domain
+
+
+def data_summary(tab="games"):
+    if tab == "games":
+        url = api_endpoint + "tables/GetBasicInforList"
+    elif tab == "group":
+        url = api_endpoint + "tables/GetGroupInforList"
+    elif tab == "staff":
+        url = api_endpoint + "tables/GetStaffInforList"
+    elif tab == "maker":
+        url = api_endpoint + "tables/GetMakerInforList"
+    elif tab == "character":
+        url = api_endpoint + "tables/GetRoleInforList"
+    elif tab == "price":
+        url = api_endpoint + "tables/GetSteamInforList"
+    elif tab == "score":
+        url = api_endpoint + "tables/GetGameScoreList"
+    else:
+        print("数据汇总就这些分类没有其他的啦！写BUG了吧！")
     pass
+
 
 def main():
     pass
+
 
 if __name__ == "__main__":
     main()
