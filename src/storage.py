27c89197dd45614c5221696faa2df117c5004497
快乐的老鼠宝宝 <keaitianxinmail@qq.com>
2023-02-21 17:10:40 +0800

    feat: save entry in db
    need to judge type, question can be seen at https://imgur.com/a/320fO5j
    
    They haven't given type id means what in https://github.com/CnGal/CnGalWebSite/blob/master/Docs/AboutCode/APIInstructions/Summary.md#%E8%AF%8D%E6%9D%A1

---
+++ src/storage.py
@@ -0,0 +1,12 @@
+# init 4 table in mongodb, "games", "staff", "article", "character"
+
+# How to judge? by "type":0/1/2/3 ?
+
+# according to my guess
+# 0 game
+# 1 character
+# 2 maker
+# 3 staff
+
+
+import pymongo
