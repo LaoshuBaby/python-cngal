544ac15e89d5fbfee760560869c5cf4c5e36048a
快乐的老鼠宝宝 <keaitianxinmail@qq.com>
2023-02-22 14:45:14 +0800

    refactor: split into functions

--- src/main.py
+++ src/main.py
@@ -15,33 +15,46 @@ def type2collection(type: int) -> str:
 
 
 def init_graph():
-    entry_meta_list = request_swagger_api("/api/entries/GetAllEntriesIdName")
-    max_limit = len(entry_meta_list) + 1
+    entry_meta_list = request_swagger_api(
+        "/api/entries/GetAllEntriesIdName"
+    )
     entry_list = {}
-    for entry_meta in entry_meta_list:
-        id = entry_meta["id"]
-        if id <= max_limit:
-            entry = request_swagger_api(
-                "/api/entries/GetEntryView/{id}".replace("{id}", str(id))
-            )
-            post_id = insert_entry(
-                entry=entry,
-                collection=init_collection(
-                    client=init_connection(),
-                    db_name="cngal",
-                    collection_name="cngal." + type2collection(entry["type"]),
-                ),
-            )
-            entry_list[id] = post_id
 
-    print(entry_list)
+    def get_data():
+        max_limit = len(entry_meta_list) + 1
+        for entry_meta in entry_meta_list:
+            id = entry_meta["id"]
+            if id <= max_limit:
+                entry = request_swagger_api(
+                    "/api/entries/GetEntryView/{id}".replace("{id}", str(id))
+                )
+                post_id = insert_entry(
+                    entry=entry,
+                    collection=init_collection(
+                        client=init_connection(),
+                        db_name="cngal",
+                        collection_name="cngal."
+                        + type2collection(entry["type"]),
+                    ),
+                )
+                entry_list[id] = post_id
 
+    def build_graph():
+        # game-maker
+        # game-staff
+        # game-character
+        import networkx as nx
 
-def build_graph():
-    # game-maker
-    # game-staff
-    # game-character
-    pass
+        G = nx.Graph()
+        for i in range(len(entry_meta_list)):
+            G.add_node(entry_meta_list[i]["id"])
+            # append entry_list[id] (contain name)
+        pass
+
+    # if cngal.config haven't got a is_finish==True, that we need to get_data
+    # do we need to select and compare before insert? if have different? delete old one or drop memory one?
+    get_data()
+    build_graph()
 
 
 def main():
