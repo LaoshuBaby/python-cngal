4e7c2fa47d97e25ea733b44d6588d4f4516b44c3
快乐的老鼠宝宝 <42690037+LaoshuBaby@users.noreply.github.com>
2023-04-01 18:23:15 +0700

    dependency: remove graph related package

--- src/pycngal/const.py
+++ src/pycngal/const.py
@@ -3,7 +3,3 @@
 api_endpoint = "https://api.cngal.org"
 headers = {"User-Agent": "python-cngal/0.0.3"}
 
-# Database
-
-db_name = "cngal"
-type_code = {0: "game", 1: "character", 2: "maker", 3: "staff"}
